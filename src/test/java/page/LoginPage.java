b1948c552b03c26d6ba69452554a5f5c21e4d8c2
Eldar <ekhismatov@list.ru>
2023-09-30 13:38:10 +0500

    Edits made

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -1,20 +1,35 @@
 package page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.example.DataHelper;
+import data.DataHelper;
 
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
     private SelenideElement loginField = $("[data-test-id=login] input");
     private SelenideElement passwordField = $("[data-test-id=password] input");
     private SelenideElement loginButton = $("[data-test-id=action-login]");
+    private SelenideElement errorMassage = $("[data-test-id='error-notification']");
+
+    public void getErrorMassage(String textError) {
+        errorMassage
+                .shouldHave(Condition.text(textError))
+                .shouldBe(visible);
+    }
 
     public VerificationPage validLogin(DataHelper.AuthInfo info) {
         loginField.setValue(info.getLogin());
         passwordField.setValue(info.getPassword());
         loginButton.click();
         return new VerificationPage();
+    }
+
+    public void invalidLoginOrPassword(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
     }
 }
--- src/test/java/page/VerificationPage.java
+++ src/test/java/page/VerificationPage.java
@@ -1,8 +1,10 @@
 package page;
 
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.example.DataHelper;
+import data.DataHelper;
+
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,14 +12,26 @@ import static com.codeborne.selenide.Selenide.$;
 public class VerificationPage {
     private SelenideElement codeField = $("[data-test-id=code] input");
     private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private SelenideElement errorMassage = $("[data-test-id='error-notification']");
 
     public VerificationPage() {
         codeField.shouldBe(visible);
+    }
+
+    public void getErrorMassage(String textError) {
+        errorMassage
+                .shouldHave(Condition.text(textError))
+                .shouldBe(visible);
     }
 
     public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
         codeField.setValue(verificationCode.getCode());
         verifyButton.click();
         return new DashboardPage();
+    }
+
+    public void invalidVerify(DataHelper.VerificationCode invalidVerificationCode) {
+        codeField.setValue(invalidVerificationCode.getCode());
+        verifyButton.click();
     }
 }
